b06e623553106d44c3baa1fdeaf46886ae4f5a0f
Margo <marglodistovar@gmail.com>
2022-07-19 20:25:41 -0400

    Cambio estilos para mejor visualizacion mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,37 +39,32 @@ async function cargarDatos()
   const colorDeLinea = "red";
  const chartDOM = document.getElementById("myChart2");
   console.log("ENtre a la funcion cargar datos")
-  fetch(dolar)
+  await fetch(dolar)
         .then(respuesta => respuesta.json())
         .then(respuestaDecodificada => {
          
-            let fechas = (respuestaDecodificada.serie).map(function (elemento) {return elemento.fecha;})
-            let etiquetas = (respuestaDecodificada.serie).map(function (etiq) {return etiq.valor;})
+            const fechas = (respuestaDecodificada.serie).map(function (elemento) {return elemento.fecha;})
+            const etiquetas = (respuestaDecodificada.serie).map(function (etiq) {return etiq.valor;})
            
         const config = {
           type: tipoDeGrafica,
           data: {
                   labels: fechas.reverse().slice(-10),
-                  datasets: [
+                  datasets: [                                           //Aqui cada objeto representa un inidcador que sera visualizado en la grafica
                                 {
                                   label: titulo,
                                   borderColor: "rgb(255,0, 132)",
                                   backgroundColor: colorDeLinea,
                                   data: etiquetas.reverse().slice(-10)
-                                  }
+                                  },
                             ]
                 }
                         };
 
               console.log(config)          
-              new Chart(chartDOM, config);
-         
+              new Chart(chartDOM, config);         
           });
-          
 }
-
-
-
 
 
 async function renderGrafica() {
@@ -104,44 +99,3 @@ function valideKey(evt) {
     return false;
   }
 }
-
-////////////////////////////////// PARA DIBUJAR LA GRAFICA
-
-
-
-// async function getAndCreateDataToChart() {
-//   const res = await
-//   fetch("https://api.gael.cloud/general/public/sismos");
-//   const sismos = await res.json();
-//   const labels = sismos.map(function (sismo) {return sismo.Fecha;});
-
-
-//   const data = sismos.map((sismo) => {
-//   const magnitud = sismo.Magnitud.split(" ")[0];
-//   return Number(magnitud);
-//   });
-
-//   console.log(sismos)
-//   console.log(data);
-  
-//   const datasets = [
-//   {
-//   label: "Sismo",
-//   borderColor: "rgb(255, 99, 132)",
-//   data
-//   }
-//   ];
-//   return { labels, datasets };
-//   }
-
-//   async function renderGrafica() {
-//     const data = await getAndCreateDataToChart();
-//     const config = {
-//     type: "line",
-//     data
-//     };
-//     const myChart = document.getElementById("myChart");
-//     myChart.style.backgroundColor = "white";
-//     new Chart(myChart, config);
-//     }
-//    // renderGrafica();
