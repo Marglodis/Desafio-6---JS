ad14b769e3aaceafb0c83c651327c8fefa9261a0
Margo <marglodistovar@gmail.com>
2022-07-21 18:40:01 -0400

    Suprimir condicionales de la función convertir.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,107 +6,119 @@ const resultado = document.getElementById("resultado");
 const tabla = document.getElementById("lista-usuario");
 const moneda = document.getElementById("moneda");
 const chartDOM = document.getElementById("myChart2").getContext("2d");
+const cantidad = document.getElementById("cantidad");
 
 document.querySelector(".fecha").innerHTML = new Date().toLocaleString();
 
 //PEtición++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 async function getMonedas(url) {
-  const endpoint = url;
   try {
-    const res = await fetch(endpoint);
+    const res = await fetch(url);
     const monedas = await res.json();
     return monedas;
   } catch (e) {
-    alert(e.message);
+    alert("Algo anda mal con la API. Intente nuevamente", e.message);
   }
 }
 /*
  ****************************************************************************************
  */
 async function convertir() {
-  if (clp.value == "") alert("Ingrese un monto válido");
+   const currency =moneda.options[moneda.selectedIndex].text.substring(0, 3); //Extraer los 3 primeros caracteres de la cadena para asignarlo al currency
+ 
+  if (clp.value == "" || isNaN(clp.value) || clp.value < 0) {
+        alert("Ingrese un monto válido");
+        limpiar();  
+  } 
   else {
-    try {
-      const divisas = await getMonedas(urlAPI);
-     
-      if (moneda.value == "dolar") 
-      {
-        resultado.innerHTML = `Resultado: ${new Intl.NumberFormat("de-DE", {style: "currency", currency: "USD",}).format((clp.value / divisas.dolar.valor).toFixed(2))}`;
-        renderGrafica();
+      try {
+        const divisas = await getMonedas(urlAPI);
+
+        resultado.innerHTML = `Resultado: ${new Intl.NumberFormat("de-DE", {
+          style: "currency", currency: currency}).format((clp.value / divisas[moneda.value].valor).toFixed(2))}`;
+
+          renderGrafica();
       } 
-      else 
-      {
-        resultado.innerHTML = `Resultado: ${new Intl.NumberFormat("de-DE", {style: "currency",currency: "EUR",}).format((clp.value / divisas.euro.valor).toFixed(2))}`;
-        renderGrafica();
+      catch (err) {
+        alert("Algo anda mal. Intente la consulta nuevamente");
       }
-    } 
-    catch (err) 
-    {
-      alert("Algo anda mal. Intente la consulta nuevamente");
-      console.log(err.message);
-    }
   }
 }
 
-//************************************************************************************************************* */
 async function cargarDatos(moneda) {
+  try {
+    const divisas = await getMonedas(urlAPI + "/" + moneda.value);
+    const fechas = divisas.serie.map((elemento) => {
+      return elemento.fecha.split("T")[0];
+    });
+    const etiquetas = divisas.serie.map((etiq) => {return etiq.valor;});
+
+    return { fechas, etiquetas };
+  } catch (e) {
+    alert("Fallo en la obtención de datos. Intente nuevamente");
+  }
+}
+
+
+/////////////////////////////////////////////////////////
+
+async function renderGrafica() {
   const tipoDeGrafica = "line";
-  const titulo = "Historico " + moneda.value.toUpperCase();
-  const colorDeLinea =  "#"+randomHex(6)+""; 
-  
-  const divisas = await getMonedas(urlAPI + "/" + moneda.value);
+  const colorBG = "#" + randomHex(6);
+  const colorDeLinea = "#" + randomHex(6);
 
-  const fechas = divisas.serie.map((elemento) => {return elemento.fecha;});
-  const etiquetas = divisas.serie.map((etiq) => {return etiq.valor;});
-
+  try {
+  const datosRender= await cargarDatos(moneda);
+  const titulo = moneda.value.toUpperCase();
+ 
   const config = {
     type: tipoDeGrafica,
     data: {
-      labels: fechas.reverse().slice(-10),
+      labels: datosRender.fechas.reverse().slice(-10),
       datasets: [
-        {                                         //Aqui cada objeto representa un indicador que sera visualizado en la grafica
+        {
+          //Aqui cada objeto representa un indicador que sera visualizado en la grafica
           label: titulo,
-          borderColor: "#"+randomHex(6)+"", 
+          borderColor: colorBG,
           backgroundColor: colorDeLinea,
-          data: etiquetas.reverse().slice(-10),
+          data: datosRender.etiquetas.reverse().slice(-10),
         },
       ],
     },
   };
 
-  //REfrescar el grafico
+  //Refrescar el grafico
 
   if (window.chartDOM) {
     window.chartDOM.clear();
     window.chartDOM.destroy();
+    
   }
+  
   window.chartDOM = new Chart(chartDOM, config);
 }
-
-async function renderGrafica() {
-   await cargarDatos(moneda);
+catch(err){
+  alert("Hubo un fallo en la carga de datos, por favor, reintente")
+}
 }
 
-moneda.addEventListener("change", function () {
-  this.options[moneda.selectedIndex];
+// moneda.addEventListener("change", () => this.options[moneda.selectedIndex]);
+
+clp.addEventListener("change",  ()=> {
+ 
+  const options2 = { style: "currency", currency: "CLP" };
+  const numberFormat2 = new Intl.NumberFormat("es-cl",options2);
+  cantidad.innerHTML = `Monto a convertir: ${numberFormat2.format(clp.value)}`
+ 
 });
-
-function valideKey(evt) {
-  // code is the decimal ASCII representation of the pressed key.
-  var code = evt.which ? evt.which : evt.keyCode;
-
-  if (code == 8) {
-    // backspace.
-    return true;
-  } else if (code >= 48 && code <= 57) {
-    // is a number.
-    return true;
-  } else {
-    // other keys.
-    return false;
-  }
-}
-
 //GEnerar los colores de lineas aleatoriamente en cada busqueda
-randomHex = length => ('0'.repeat(length) + Math.floor((Math.random() * 16 ** length)).toString(16)).slice(-length);
+randomHex = (length) => ("0".repeat(length) + Math.floor(Math.random() * 16 ** length).toString(16)).slice(-length);
+
+function limpiar()
+{
+        clp.value = "";
+        resultado.innerHTML="Resultado: ";
+        cantidad.innerHTML="Monto a convertir: ";
+        window.chartDOM.clear();       
+}
